3ce9f79425f8d45740de2416fadf65bc92f9aaa7
Alex <aleksei.golovanov@simbirsoft.com>
2022-12-06 13:19:29 +0300

    add metods

--- src/test/java/com/golovanov/tests/Animal.java
+++ src/test/java/com/golovanov/tests/Animal.java
@@ -1,23 +1,33 @@
 package com.golovanov.tests;
 
 public class Animal {
-int id;
-String name;
-String neededFor;
-boolean benefit;
+    int id;
+    String name;
+    String neededFor;
+    int weight;
+    int trick;
+    boolean benefit;
 
-
-    public Animal(int id, String name, String neededFor, boolean benefit) {
+    public Animal(int id, String name, String neededFor, int weight, int trick, boolean benefit) {
         this.id = id;
         this.name = name;
         this.neededFor = neededFor;
+        this.weight = weight;
+        this.trick = trick;
         this.benefit = benefit;
     }
-
-    int tochangeid (int newid){
-        int result=id=newid;
+    int tochangeid(int newid) {
+        int result = id = newid;
         return result;
     }
-
-
+    int toFeed(int feed) {
+        if (benefit == true) {
+            weight += feed;
+            System.out.println("Ты хорошо работал, поешь!" );
+        } else {
+            System.out.println("Ничего не получишь, бездельник!");
+        }
+        return weight;
+        }
 }
+
--- src/test/java/com/golovanov/tests/AnimalTest.java
+++ src/test/java/com/golovanov/tests/AnimalTest.java
@@ -1,12 +1,36 @@
 package com.golovanov.tests;
 
+import java.beans.DefaultPersistenceDelegate;
 public class AnimalTest {
 
+    private static String revenue;
     public static void main(String[] args) {
-        Animal dog = new Animal(1,"Тузик", "security",true);
-        Animal bird = new Animal(1,"Гусь", "meal", true);
-        Animal cat = new Animal(1,"Мурзик", "decorative", true);
-        Animal horse = new Animal(1,"Граф", "worker", true);
-        Animal mouse = new Animal(1,"Мики", " slacker", false);
+        Animal dog = new Animal(1, "Тузик", "security", 5, 0, true);
+        Animal bird = new Animal(2, "Гусь", "meal", 3, 0, true);
+        Animal cat = new Animal(3, "Васька", "decorative", 4, 1, true);
+        Animal horse = new Animal(4, "Граф", "worker", 150, 0, true);
+        Animal mouse = new Animal(5, "Микки", " slacker", 1, 9, false);
+
+        dog.toFeed(1);
+        System.out.println("Теперь " + dog.name + " весит  " + dog.weight + " кг");
+        bird.toFeed(0);
+        System.out.println("Теперь " + bird.name + " весит  " + bird.weight + " кг");
+        mouse.toFeed(1);
+        System.out.println("Мышь осталась в весе " + mouse.weight + " кг");
+        System.out.println();
+        System.out.println("Васька, иди ловить мышей!");
+
+        for (int i = cat.trick; i <= mouse.trick+1; i++) {
+          toCatchMouse (mouse, cat);
+            cat.trick++;
+        }
+    }
+   static void toCatchMouse(Animal animal1, Animal animal2) {
+        if (animal1.trick >= animal2.trick) {
+            System.out.println( "Опять неудача....");
+        } else {
+            System.out.println("Ура! Мышь поймана!");
+        }
     }
 }
+
